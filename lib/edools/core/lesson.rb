6efd78026ceb5e460c3793ff970286bd294cc2fe
Hugo Tamaki <hugokt2005@yahoo.com.br>
2014-10-13 17:38:38 -0300

    adding more models to edools-api

---
+++ lib/edools/core/lesson.rb
@@ -0,0 +1,15 @@
+require 'edools/core/base'
+require 'edools/core/paginated_collection'
+
+module Edools
+  module Core
+    class Lesson < Edools::Core::Base
+      # def self.collection_path(prefix_options = {}, query_options = nil)
+      #   super
+      #   library_id = prefix_options[:library_id] || query_options.try(:[], :library_id)
+      #   "/#{collection_name}.#{format.extension}#{query_string(query_options)}"
+      # end
+    end
+  end
+end
+
--- lib/edools/initialization.rb
+++ lib/edools/initialization.rb
@@ -1,7 +1,8 @@
 require 'active_resource'
 
 module Edools
-  CORE_BASE_URL = 'https://core.edools.com'
+  # CORE_BASE_URL = 'https://core.edools.com'
+  CORE_BASE_URL = 'http://localhost:3004'
   CORE_VERSION = 'vnd.edools.core.v1'
 
   @@credentials = nil
